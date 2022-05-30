b7b4885454ef0af23dd7120401d3119272cfc64f
luis.peralta <luis_peralta_000@hotmail.com>
2022-05-30 10:34:13 -0300

    changes with less tcs

--- creationTcs/src/AutomateLines.java
+++ creationTcs/src/AutomateLines.java
@@ -38,11 +38,10 @@ public class AutomateLines {
         //---------------------------------------------------------------
         //For write a text
         try{
-        int quantityTestCases = Integer.parseInt(args[1]);
-
-
-        int toHereTcs = Integer.parseInt(args[2]);
         Boolean isRequired = Boolean.parseBoolean(args[0].toLowerCase());
+        int quantityTestCases = Integer.parseInt(args[1]);
+        int toHereTcs = Integer.parseInt(args[2]);
+
         int argNumber = 3;
         int changeTypeTcs = 1;
         String testCase = "";
@@ -64,24 +63,10 @@ public class AutomateLines {
                         "\tY selecciono el método \n" +
                         "\tY envío la petición\n" +
                         "\tEntonces obtengo un código de respuesta 400\n\n";
-            } else if (changeTypeTcs == 2) {
-                testCase = "Validar los valores límites del Pattern, mínimos y máximos del campo <" + headerValidation.fields[argNumber] + "> y que muestre el código de respuesta 200" ;
-                gherkin = "\n\tDado que se enviará una solicitud a la api\n" +
-                        "\tCuando completo el campo mencionado con un valor validando Pattern, mínimos y máximos\n" +
-                        "\tY selecciono el método \n" +
-                        "\tY envío la petición\n" +
-                        "\tEntonces obtengo un código de respuesta 200\n\n";
-            } else if (changeTypeTcs == 3 && isRequired == true) {
+            } else if (changeTypeTcs == 2 && isRequired == true) {
                 testCase = "Validar que al ingresar NULL en el campo <" + headerValidation.fields[argNumber] + "> este muestre el código de error 400";
                 gherkin = "\n\tDado que se enviará una solicitud a la api\n" +
                         "\tCuando completo el campo mencionado con un valor NULL\n" +
-                        "\tY selecciono el método \n" +
-                        "\tY envío la petición\n" +
-                        "\tEntonces obtengo un código de respuesta 400\n\n";
-            } else if (changeTypeTcs == 4 && isRequired == true) {
-                testCase = "Validar que al ingresar vacío en el campo <" + headerValidation.fields[argNumber] + "> este muestre el código de error 400" ;
-                gherkin = "\n\tDado que se enviará una solicitud a la api\n" +
-                        "\tCuando completo el campo mencionado con un valor vacío\n" +
                         "\tY selecciono el método \n" +
                         "\tY envío la petición\n" +
                         "\tEntonces obtengo un código de respuesta 400\n\n";
